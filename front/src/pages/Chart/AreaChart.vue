0422d2906ad139924b5061a9e17f7a8eadc29445
addict3d <jean-baptiste.schmidt@epitech.eu>
2021-11-02 14:58:28 +0100

    frontend

---
+++ front/src/pages/Chart/AreaChart.vue
@@ -0,0 +1,66 @@
+<script>
+import { Line } from "vue-chartjs";
+
+export default {
+    extends: Line,
+    data() {
+        return {
+            gradient: null,
+            gradient2: null
+        };
+    },
+    mounted() {
+        this.gradient = this.$refs.canvas
+            .getContext("2d")
+            .createLinearGradient(0, 0, 0, 450);
+        // this.gradient2 = this.$refs.canvas
+            //   .getContext("2d")
+            //   .createLinearGradient(0, 0, 0, 450);
+
+        this.gradient.addColorStop(0, "rgba(0, 255,100, 0.5)");
+        this.gradient.addColorStop(0.5, "rgba(0, 255, 100, 0.25)");
+        // this.gradient.addColorStop(1, "rgba(0, 255, 100, 0)");
+
+        // this.gradient2.addColorStop(0, "rgba(0, 231, 255, 0.9)");
+        // this.gradient2.addColorStop(0.5, "rgba(0, 231, 255, 0.25)");
+        // this.gradient2.addColorStop(1, "rgba(0, 231, 255, 0)");
+
+        this.renderChart(
+        {
+            labels: [
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "october",
+                "November",
+                "December"
+            ],
+            datasets: [{
+                label: "Data One",
+                borderColor: "#2dce89",
+                pointBackgroundColor: "#cecece",
+                borderWidth: 1,
+                pointBorderColor: "#7f7f7f",
+                backgroundColor: this.gradient,
+                data: [25, 63, 98, 16, 46, 73, 52, 96, 22, 75, 26, 12, 68]
+            },
+            //   {
+            //     label: "Data Two",
+            //     borderColor: "#05CBE1",
+            //     pointBackgroundColor: "white",
+            //     pointBorderColor: "white",
+            //     borderWidth: 1,
+            //     backgroundColor: this.gradient2,
+            //     data: [60, 55, 32, 10, 2, 12, 53]
+            //   }
+            ]
+        }, { responsive: true, maintainAspectRatio: false });
+    }
+};
+</script>
---
+++ front/src/pages/Profile.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="row">
+    <div class="col-md-4">
+      <user-card :user="user"></user-card>
+      <user-report-card :user="user"></user-report-card>
+
+    </div>
+    <div class="col-md-8">
+      <edit-profile-form :user="user" @editUser="editUser" @deleteUser="deleteUser"> </edit-profile-form>
+    </div>
+  </div>
+</template>
+<script>
+import EditProfileForm from "./Profile/EditProfileForm";
+import UserCard from "./Profile/UserCard";
+import UserReportCard from "./Profile/UserReportCard";
+
+import axios from "axios";
+
+const apiEndPoint = "http://localhost:4000/api/";
+export default {
+  components: {
+    EditProfileForm,
+    UserCard,
+    UserReportCard
+  },
+  data() {
+    return {
+      model: {},
+      user: {},
+    };
+  },
+  created() {
+    axios
+      .get(`${apiEndPoint}/1`)
+      .then((response) => {
+        // JSON responses are automatically parsed.
+
+        this.user = response.data.data;
+        console.log(this.user);
+      })
+      .catch((e) => {
+        this.errors.push(e);
+      });
+  },
+  methods: {
+    editUser: function () {
+      console.log(this.user);
+      let objectUser = {
+        user: this.user,
+      };
+      delete objectUser.user.id;
+      axios
+        .put(`${apiEndPoint}/1`, objectUser)
+        .then((response) => {
+          alert("Profile edited");
+        })
+        .catch((e) => {
+          this.errors.push(e);
+        });
+    },
+    deleteUser: function () {
+      if (
+        confirm(
+          "You are going to definetely delete your account, do you want to continue?"
+        )
+      ) {
+        axios
+          .delete(`${apiEndPoint}/1`)
+          .then((response) => {
+            alert("User deleted");
+          })
+          .catch((e) => {
+            this.errors.push(e);
+          });
+      }
+    },
+  },
+};
+</script>
+<style>
+</style>
