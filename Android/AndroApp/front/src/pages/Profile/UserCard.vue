40a0c65b3f6b2e678ebc4b59aec4f661be70c189
Lenoss <45673827+Lenoss@users.noreply.github.com>
2021-11-09 15:25:50 +0100

    ADD- basic applicaton without Auth

---
+++ Android/AndroApp/front/src/pages/Profile/UserCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <card type="user">
+    <p class="card-text">
+    </p>
+    <div class="author">
+
+        <!-- <img class="avatar" src="img/anime6.png" alt="..."> -->
+        <h5 class="title">{{user.role}}</h5>
+      
+      <!-- <p class="description">
+        {{user.title}}
+      </p> -->
+    </div>
+    <p></p>
+    <!-- <p class="card-description">
+      {{user.description}}
+    </p> -->
+    <!-- <div slot="footer" class="button-container">
+      <base-button icon round class="btn-facebook">
+        <i class="fab fa-facebook"></i>
+      </base-button>
+      <base-button icon round class="btn-twitter">
+        <i class="fab fa-twitter"></i>
+      </base-button>
+      <base-button icon round class="btn-google">
+        <i class="fab fa-google-plus"></i>
+      </base-button>
+    </div> -->
+  </card>
+</template>
+<script>
+  export default {
+    props: {
+      user: {
+        type: Object,
+        default: () => {
+          return {};
+        }
+      }
+    }
+  }
+</script>
+<style>
+</style>
---
+++ Android/AndroApp/www/precache-manifest.f42bb30256a9a71684d239f07a9c8bfe.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82afbfaa2a64cebfd0077f73abe15659",
+    "url": "apple-icon.png"
+  },
+  {
+    "revision": "9a3643fbc29a15fcd263",
+    "url": "css/app.f094603c.css"
+  },
+  {
+    "revision": "8e22925c24e25efec42c",
+    "url": "css/common.a284c550.css"
+  },
+  {
+    "revision": "f8b99beaf86332c0ee8b",
+    "url": "css/dashboard.5a7b05a8.css"
+  },
+  {
+    "revision": "996d8248f580f8e26e6c45c67da9b5a6",
+    "url": "favicon.png"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "fonts/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "fonts/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "fonts/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "fonts/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "3b91359dcf1cf96c8440a7532ac61dda",
+    "url": "img/anime3.png"
+  },
+  {
+    "revision": "2a5a031c63d290c6cd3803e21d8c3b5d",
+    "url": "img/anime6.png"
+  },
+  {
+    "revision": "d2caaad36a395104bd99f30487798d79",
+    "url": "img/apple-icon.png"
+  },
+  {
+    "revision": "9b0c49b657dd738a77a37e7dabcc0b5d",
+    "url": "img/bg5.jpg"
+  },
+  {
+    "revision": "d25412bcd9af1fb98017c8144a4e40d1",
+    "url": "img/default-avatar.png"
+  },
+  {
+    "revision": "fadc3f3a4c94b151d8b8ed33456c2344",
+    "url": "img/emilyz.jpg"
+  },
+  {
+    "revision": "4422794dfa779751326660ae681b082e",
+    "url": "img/favicon.png"
+  },
+  {
+    "revision": "f220d1815d421bfa192ee352780ba194",
+    "url": "img/img_3115.jpg"
+  },
+  {
+    "revision": "40fdb6e1e72e21ff904da682c46d0116",
+    "url": "img/james.jpg"
+  },
+  {
+    "revision": "93f1328bd264f02aacef8b0c6b498445",
+    "url": "img/mike.jpg"
+  },
+  {
+    "revision": "6747f5430007b0c7fcf695fa9f47cd86",
+    "url": "index.html"
+  },
+  {
+    "revision": "9a3643fbc29a15fcd263",
+    "url": "js/app.f6811d29.js"
+  },
+  {
+    "revision": "6597860654c49a02b6ec",
+    "url": "js/chunk-vendors.1adc33f8.js"
+  },
+  {
+    "revision": "8e22925c24e25efec42c",
+    "url": "js/common.5a5a8ac6.js"
+  },
+  {
+    "revision": "40d834db3945e5a1d86f",
+    "url": "js/common~dashboard.fd4f6233.js"
+  },
+  {
+    "revision": "f8b99beaf86332c0ee8b",
+    "url": "js/dashboard.761b1df2.js"
+  },
+  {
+    "revision": "1a167cfd82a1a7480ea601fccc8d7e18",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
